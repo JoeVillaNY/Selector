235ae8aae5ff06776eecddb7201afa8454f6a746
unknown <villanyc0@gmail.com>
2019-03-06 20:44:48 -0500

    second

---
+++ src/Shape.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shape = (props) => {
+  var shape = props.shape;
+  var selectShape = props.selectShape;
+  return (
+      <div className={shape} onClick={() => selectShape(shape)}/>
+  )
+}
+
+export default Shape;
